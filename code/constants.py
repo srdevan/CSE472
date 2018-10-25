934efd6f32703a0ea2c4153bdadc3e5421770cad
Shivam Dhar <sdhar3@asu.edu>
2018-10-25 00:14:37 -0700

    #1 adds data parser module along with tweettube dataset

--- code/constants.py
+++ code/constants.py
@@ -1,4 +1,5 @@
 
+TWEETTUBE_DIR_PATH_REGEX = "../dataset/tweettube_dataset/*"
 YOUTUBE_PREFIX_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&key="
 YOUTUBE_PREFIX_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&key="
 YOUTUBE_PREFIX_COMMENT_THREAD_URL = "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&key="
