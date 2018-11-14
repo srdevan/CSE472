c44b6022c0dc7c10fb4a785c65f9d135e09e8fea
Shivam Dhar <sdhar3@asu.edu>
2018-11-13 23:00:55 -0800

    Adds feature functions for model

--- code/feature_functions.py
+++ code/feature_functions.py
@@ -1,313 +1,159 @@
-"""
+"""-
 bot_inflated
 organic
 """
+import bot_users
 import re
+
 def f1(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["gg ggy"]
+			user_ids = bot_users.users
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i] == j[0].lower()):	
+			for i in range(len(user_ids)):
+				if(j[0].lower() in user_ids[i]):
 					return 1
 	return 0
+
 
 def f2(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["RGARMY.HATEPAGE"]
+			patterns = ['easy way to earn money', 'earn money', 'free money', 'earn easy money', 'make money online',
+						'earn online', 'earn money online', 'get money']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f3(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["Robert Oeser"]
+			patterns = ['watch and share', 'please share']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f4(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["500 Subscribers without A video Challenge?"]
+			patterns = ['my referral code', 'referral code']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f5(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["Top Hater"]
+			patterns = ['visit this site', 'take a look at', 'check out', 'please see', 'go to', 'follow the link',
+						'plz go through']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f6(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["Hawkeyefan95IA"]
+			patterns = ['donate', 'need of support', 'help this guy', 'giveaways']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f7(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["The Dominique Harris Channel"]
+			patterns = ['my link', 'click on the link', 'download link']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f8(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["brave nimz"]
+			patterns = ['credit service']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f9(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["thaaffguy"]
+			patterns = ['shoot me an email']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f10(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["Nadine Bowling"]
+			patterns = ['dot com']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f11(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["Bozica Ristova"]
+			patterns = ['facebook', 'instagram']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f12(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["akthercho"]
+			patterns = ['action now']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f13(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["velimirka zusamen"]
+			patterns = ['subscribe me', 'comment and subscribe', 'follow us', 'subscribe']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f14(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["kutti kavi"]
+			patterns = ['discount']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
 	return 0
 
 def f15(sentence, tag):
 	for comment in sentence:
 		if(tag == "bot_inflated"):
-			s = ["theCVman"]
+			patterns = ['invite']
 			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
+			for i in range(len(patterns)):
+				if(re.findall(patterns[i], j[1].lower())):
 					return 1
-	return 0
-
-def f16(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Matty Marz"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f17(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["PXRRIS DANIEL"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f18(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Brad Cowen"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f19(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Connor Lexington"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f20(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Ravi Bedia"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f21(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["IZMCreditServices.net"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f22(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Melanee Davis"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f23(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["Krassi Popov"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f24(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["castaway140"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i] == j[0]):	
-					return 1
-	return 0
-
-def f25(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["John David"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f26(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["MistafloX MistafloX"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f27(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["NewEraCap50"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f28(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["MrLifeChat"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f29(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			s = ["ChuckTaylormfc05"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() == j[0].lower()):	
-					return 1
-	return 0
-
-def f30(sentence, tag):
-	for comment in sentence:
-		if(tag == "organic"):
-			s = ["ChuckTaylormfc05"]
-			j = comment.split("\t")
-			for i in range(len(s)):
-				if(s[i].lower() != j[0].lower()):	
-					return 1
-	return 0
-
-def f31(sentence, tag):
-	for comment in sentence:
-		if(tag == "bot_inflated"):
-			pattern = "https://"
-			j = comment.split("\t")
-			if(re.search(pattern, j[1])):	
-				return 1
 	return 0
